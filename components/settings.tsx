eb20fa04a968b21cc6ccde67bad4257cd4efa51f
Dylan Awalt-Conley <web@dylan.ac>
2022-10-25 12:25:01 -0400

    fixup! refactor to avoid useCallback, risky useEffect

--- components/settings.tsx
+++ components/settings.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import { Button, Toggle } from './button'
 import { Word } from '../data/wordlist'
 
