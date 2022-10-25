de564e941eaab43b40abc1daf2f0bec83087e9bb
Dylan Awalt-Conley <web@dylan.ac>
2022-10-25 19:23:47 -0400

    move ScoreDisplay into its own file

---
+++ components/score.tsx
@@ -0,0 +1,29 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faThumbsUp, faThumbsDown } from '@fortawesome/pro-light-svg-icons'
+
+interface ScoreDisplayProps {
+  correct: number
+  incorrect: number
+  remaining: number
+}
+const ScoreDisplay = ({ correct, incorrect, remaining }: ScoreDisplayProps) => (
+  <>
+    <span className="text-theme-green">
+      <span>{correct}</span>
+      <FontAwesomeIcon
+        icon={faThumbsUp}
+        className="align-text-middle ml-1 inline-block aspect-square h-[0.9em]"
+      />
+    </span>
+    <span className="text-theme-red">
+      <span>{incorrect}</span>
+      <FontAwesomeIcon
+        icon={faThumbsDown}
+        className="align-text-middle ml-1 inline-block aspect-square h-[0.9em]"
+      />
+    </span>
+    <span>{remaining} remaining</span>
+  </>
+)
+
+export { ScoreDisplay }
